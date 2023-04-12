812cc4dec013fcdf5242bc1d118e8a0a4ef53230
Artem Lubentsov <artem.law@mail.ru>
2023-04-12 20:40:13 +0300

    Add task №2 result

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+import requests
+import os
+
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload(self, file_path: str, disk_folder_path: str = None):
+        # Если путь к файлу указан
+        if disk_folder_path:
+            disk_file_path = f'{disk_folder_path}/{os.path.basename(file_path)}'
+        # Иначе пишем в корень диска
+        else:
+            disk_file_path = os.path.basename(file_path)
+
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(file_path, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+            return 'Success'
+        else:
+            return 'Error'
 
 
 if __name__ == '__main__':
